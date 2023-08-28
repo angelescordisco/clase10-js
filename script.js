0c0776f2c76cdfc3c2230821f6bb887897aa837c
angelescordisco <93674455+angelescordisco@users.noreply.github.com>
2023-08-27 23:10:26 -0300

    clase10 javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let nombre= "Angeles";
+let apellido= "Cordisco";
+console.log(nombre+""+apellido)
+
+var number1 = 12000;
+var number2 = 20;
+var suma = number1 + number2;
+console.log("suma:",suma);
+
+var number3= 400;
+var suma = number1 + number2 + number3;
+console.log("suma:",suma);
